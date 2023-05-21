aa807610699624f23f69bf341c7c5482a0f00712
alimss18 <134178365+alimss18@users.noreply.github.com>
2023-05-21 03:03:05 +0200

    Delete Fichiers directory

--- Fichiers/wp/db.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS dbwordpress;
-CREATE USER IF NOT EXISTS 'wpuser'@'%' IDENTIFIED WITH mysql_native_password BY 'wpmdp';
-GRANT ALL PRIVILEGES ON dbwordpress.* TO 'wpuser'@'%' IDENTIFIED BY 'wpmdp';
-FLUSH PRIVILEGES;
-QUIT;
